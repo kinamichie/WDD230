959ef8ebbfc77af58a646e45dd084be6fa1f6b4c
Kina <mic18019@byui.edu>
2020-05-23 12:46:57 -0800

    commit

--- assignments/todo-list/utilities.js
+++ assignments/todo-list/utilities.js
@@ -3,17 +3,18 @@ function renderTodoList(parent,todos) {}
 
 function renderOneTodo(todo) {
     const item = document.createElement('li');
+    item.innerHTML = `
     id.innerHTML = <li>${todo.id}</li>
     content.innerHTML = <li>${todo.content}</li>
-    completed.innerHTML = <li>${todo.completed}</li> 
+    completed.innerHTML = <li>${todo.completed}</li> `
     return item; 
 }
 function renderOneTodoFull(todo){
     const item = document.createElement('li');
-    
+    item.innerHTML = `
     id.innerHTML = <li>${todo.id}</li>
     content.innerHTML = <li>${todo.content}</li>
-    completed.innerHTML = <li>${todo.completed}</li> 
+    completed.innerHTML = <li>${todo.completed}</li> `
     ;
     parent.innerHTML = '';
     parent.appendChild(item);
