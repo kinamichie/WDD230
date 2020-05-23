e1533c9dce3ea05e6f15490e74ecaa2e475f329f
Kina <mic18019@byui.edu>
2020-05-23 12:42:47 -0800

    commit

--- assignments/todo-list/utilities.js
+++ assignments/todo-list/utilities.js
@@ -1,1 +1,22 @@
 //Create DOM manipulation helper functions
+function renderTodoList(parent,todos) {}
+
+function renderOneTodo(todo) {
+    const item = document.createElement('li');
+    id.innerHTML = <li>${todo.id}</li>
+    content.innerHTML = <li>${todo.content}</li>
+    completed.innerHTML = <li>${todo.completed}</li> 
+    return item; 
+}
+function renderOneTodoFull(todo){
+    const item = document.createElement('li');
+    
+    id.innerHTML = <li>${todo.id}</li>
+    content.innerHTML = <li>${todo.content}</li>
+    completed.innerHTML = <li>${todo.completed}</li> 
+    ;
+    parent.innerHTML = '';
+    parent.appendChild(item);
+    }
+
+export default utilities;
