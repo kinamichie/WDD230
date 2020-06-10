41d99d9e7b809d68e31180ab344f327efd6be803
Kina <mic18019@byui.edu>
2020-06-10 10:39:35 -0800

    update

--- assignments/todo-list/ls.js
+++ assignments/todo-list/ls.js
@@ -21,7 +21,7 @@ function completedTodo(){
 function setCompleted(id, value){
     const toDoList = getTodoList();
 
-    //const updatedTodos = toDoList.filter( todo => todo.id != id);
+    const updatedTodos = toDoList.filter( todo => todo.id != id);
     toDoList.completed = value;
     localStorage.setItem('toDoList', JSON.stringify(updatedTodos));
     
